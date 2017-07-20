b8407391e6e92096e782510c5ec055d44d634f78
TobiasAndre <tobiasae@gmail.com>
2017-07-20 20:48:14 -0300

    * Implement report screen

--- app/src/main/java/com/tobiasandre/goestetica/ui/ReportFragment.java
+++ app/src/main/java/com/tobiasandre/goestetica/ui/ReportFragment.java
@@ -1,10 +1,16 @@
 package com.tobiasandre.goestetica.ui;
 
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +19,11 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.tobiasandre.goestetica.R;
+import com.tobiasandre.goestetica.database.GoEsteticaContract;
 import com.tobiasandre.goestetica.ui.Adapter.ReportAdapter;
+import com.tobiasandre.goestetica.ui.Adapter.ScheduleAdapter;
+
+import java.util.Date;
 
 /**
  * Created by TobiasAndre on 22/06/2017.
@@ -23,11 +33,17 @@ public class ReportFragment extends Fragment implements
         LoaderManager.LoaderCallbacks<Cursor>,
         ReportAdapter.Callbacks{
 
+    private static String TAG = ReportFragment.class.getSimpleName();
+    public static final int ID_REPORT_LOADER = 44;
+    private int mPosition = RecyclerView.NO_POSITION;
+    Uri contentUri = GoEsteticaContract.ScheduleEntry.CONTENT_URI;
     View rootView;
     int idCustomer,idTreatment;
     TextView tvDtInit,tvDtFin;
     Spinner spnReportType,spnResultType;
-    ImageButton btnGenerate;
+    ImageButton btnGenerate,btnFindTreatment,btnFindCustomer;
+    private RecyclerView mRecyclerView;
+    private ReportAdapter mAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,6 +64,42 @@ public class ReportFragment extends Fragment implements
         spnReportType = (Spinner)rootView.findViewById(R.id.spnReportType);
         spnResultType = (Spinner)rootView.findViewById(R.id.spnResultType);
         btnGenerate = (ImageButton)rootView.findViewById(R.id.btnRunReport);
+        btnFindCustomer = (ImageButton)rootView.findViewById(R.id.btn_find_customer);
+        btnFindTreatment = (ImageButton)rootView.findViewById(R.id.btn_find_treatment);
+
+        LinearLayoutManager layoutManager =
+                new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new ReportAdapter(getContext(), (ReportAdapter.Callbacks) getContext());
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        tvDtInit.setText(new Date().toString());
+        tvDtFin.setText(new Date().toString());
+
+        if(btnFindTreatment!=null){
+            btnFindTreatment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intentListTreatment = new Intent(getContext(),TreatmentListActivity.class);
+                    startActivityForResult(intentListTreatment,2);
+                }
+            });
+        }
+
+        if(btnFindCustomer!=null){
+            btnFindCustomer.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intentListCustomer = new Intent(rootView.getContext(),CustomerListActivity.class);
+                    startActivityForResult(intentListCustomer,3);
+                }
+            });
+        }
 
         if(btnGenerate!=null){
             btnGenerate.setOnClickListener(new View.OnClickListener() {
@@ -58,27 +110,72 @@ public class ReportFragment extends Fragment implements
             });
         }
 
+        getActivity().getSupportLoaderManager().initLoader(ID_REPORT_LOADER, null, this);
+
         return rootView;
     }
 
     private void generateReport() {
 
 
+        getActivity().getSupportLoaderManager().initLoader(ID_REPORT_LOADER, null, this);
+
     }
 
     @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==2){
+            if(data!=null) {
+                Bundle res = data.getExtras();
+                if (res.containsKey("id")) {
+                    idTreatment = res.getInt("id");
+                }
+            }
+        }else if (requestCode == 3){
+            if(data!=null) {
+                Bundle res = data.getExtras();
+                if (res.containsKey("id")) {
+                    idCustomer = res.getInt("id");
+                }
+            }
+        }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle bundle) {
+        String filter = "";
+        if(bundle!=null){
+            filter = bundle.getString("filter");
+        }
+        filter = GoEsteticaContract.CustomerEntry.COLUMN_CUSTOMER_NAME + " like '%"+filter+"%' ";
+        return new CursorLoader(getActivity(),contentUri,null,filter,null,null);
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if(mAdapter!=null && data!=null) {
+            mAdapter.swapCursor(data);
 
+            if (mPosition == RecyclerView.NO_POSITION){
+                mPosition = 0;
+            }
+
+            mRecyclerView.smoothScrollToPosition(mPosition);
+
+        }else{
+            Log.v(TAG,"OnLoadFinished: mAdapter is null");
+        }
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-
+        if(mAdapter!=null)
+            mAdapter.swapCursor(null);
+        else {
+            Log.v(TAG, "OnLoadFinished: mAdapter is null");
+        }
     }
 
     @Override
